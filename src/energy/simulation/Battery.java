919a898b8601648c6d46a8ab7943227e8ee553df
SkuliSkula <skuliskula84@gmail.com>
2017-11-02 13:11:55 +0100

    Created classes for the implementation

--- src/energy/simulation/Battery.java
+++ src/energy/simulation/Battery.java
@@ -1,5 +1,35 @@
 package energy.simulation;
 
+/*This class will simulate all batteries in this project*/
 public class Battery {
-
+	private double capacity;
+	private String batteryBrand;
+	private boolean isCharging;
+	private double status; // How much kW does the battery currently have
+	
+	public Battery(double capacity) {
+		this.capacity = capacity;
+		this.batteryBrand = "";
+	}
+	
+	public Battery(double capacity, String brand) {
+		this.capacity = capacity;
+		this.batteryBrand = brand;
+	}
+	
+	public boolean isCharging() {
+		return this.isCharging;
+	}
+	
+	public boolean isFull() {
+		return this.capacity == this.status; // For simplicity just check the capacity equals the status
+	}
+	
+	public void setBatteryBrand(String brand) {
+		this.batteryBrand = brand;
+	}
+	
+	public String getBatteryBrand() {
+		return this.batteryBrand;
+	}
 }
--- src/energy/simulation/ElectricCar.java
+++ src/energy/simulation/ElectricCar.java
@@ -1,5 +1,13 @@
 package energy.simulation;
 
+/*This class will simulate Tesla Model S P100D*/
 public class ElectricCar {
-
+	private static final double BATTERY_CAPACITY = 100; // This is 100kW
+	private Battery carBattery;
+	
+	public ElectricCar() {
+		this.carBattery = new Battery(BATTERY_CAPACITY);
+	}
+	
+	
 }
