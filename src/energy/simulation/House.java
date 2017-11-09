54b8059541cbb91c8ec11df8c66beda7edd05dde
stookit <mardari.adri@gmail.com>
2017-11-09 15:50:09 +0100

    House

--- src/energy/simulation/House.java
+++ src/energy/simulation/House.java
@@ -1,6 +1,5 @@
 package energy.simulation;
 
-import GenCol.Queue;
 import GenCol.entity;
 import model.modeling.message;
 import view.modeling.ViewableAtomic;
@@ -8,9 +7,10 @@ import view.modeling.ViewableAtomic;
 public class House extends ViewableAtomic{
 	
 	protected double time;
-	protected int consumed;
+	protected double consumed;
 	protected entity ent;
 	protected int genVal;
+	protected Energy en;
 	
 	public House(){
 		this("house");
@@ -21,7 +21,7 @@ public class House extends ViewableAtomic{
 		
 		addInport("in");
 		
-		addTestInput("in", new Energy("banana", 500));
+		addTestInput("in", new Energy(500));
 	}
 	
 	public void initialize() {
@@ -32,7 +32,7 @@ public class House extends ViewableAtomic{
 	}
 	
 	public void deltint() {
-		clockTime += sigma;
+		time += sigma;
 	}
 	
 	//I receive 0 or smth
@@ -40,105 +40,29 @@ public class House extends ViewableAtomic{
 		time += e;
 		Continue(e);
 
-		if (phaseIs("passive"))
+		if (phaseIs("active"))
 			for (int i = 0; i < x.getLength(); i++) {
 				if (messageOnPort(x, "in", i)) {
-					customer c = (customer) x.getValOnPort("in", i);
-					c.service_time = clockTime;
-					this.customer = c;
-					holdIn("busy", c.processing_time);
+					Energy energ = (Energy) x.getValOnPort("in", i);
+					System.out.println("Energy received in the house: " + energ.getEnergy());
+					//Maybe add the energy in later iterations, not just instantiate it
+					this.en = energ;
+					this.consumed += energ.getEnergy();
 					return;
 				}
-
-				if (messageOnPort(x, "q_status", i)) {
-					signal q_status = (signal) x.getValOnPort("q_status", i);
-
-					if (q_status.name.equals("1")) {
-						holdIn("calling", 20);
-					}
-				}
 			}
-
+		System.out.println("So far, the house has consumed " + consumed + " energies");
 		System.out.println("external-Phase after: " + phase);
 	}
 	
-	
-	
-	
-	////////////////////////////////////
-	protected customer customer;
-	protected Queue cq;
-	protected int noCustomerInQueue;
-	protected double clockTime;
-
-	public teller() {
-		this("teller");
-	}
-
-	public teller(String name) {
-		super(name);
-		addInport("in");
-		addOutport("next");
-		addOutport("out");
-		addInport("q_status");
-
-		addTestInput("in", new customer("C1", 1000));
-		addTestInput("in", new customer("C2", 2000));
-		addTestInput("in", new customer("C3", 1500));
-		addTestInput("in", new customer("C4", 4000));
-		addTestInput("q_status", new signal("1"));
-		addTestInput("q_status", new signal("0"));
-	}
-
-	public void deltext1(double e, message x) {
-		clockTime += e;
-		Continue(e);
-
-		if (phaseIs("passive"))
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "in", i)) {
-					customer c = (customer) x.getValOnPort("in", i);
-					c.service_time = clockTime;
-					this.customer = c;
-					holdIn("busy", c.processing_time);
-					return;
-				}
-
-				if (messageOnPort(x, "q_status", i)) {
-					signal q_status = (signal) x.getValOnPort("q_status", i);
-
-					if (q_status.name.equals("1")) {
-						holdIn("calling", 20);
-					}
-				}
-			}
-
-		System.out.println("external-Phase after: " + phase);
-	}
-
-	
-
 	public void deltcon(double e, message x) {
 		deltint();
 		deltext(0, x);
 	}
 
-	public message out() {
-		message m = new message();
-
-		if (phaseIs("calling")) {
-			m.add(makeContent("next", (signal) new signal("1")));
-		}
-		if (phaseIs("busy")) {
-			this.customer.depart_time = clockTime;
-			m.add(makeContent("out", (customer) this.customer));
-		}
-		return m;
-	}
-
 	public void showState() {
 		super.showState();
-		// System.out.println("job: " + job.getName());
+		//System.out.println("job: " + job.getName());
 	}
 
 	public String getTooltipText() {
