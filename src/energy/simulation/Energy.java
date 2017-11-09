5eb2064b706321d1d191e5b3005c23e78eef3cb4
ZuzkaJelcicova <215107@via.dk>
2017-11-09 15:30:14 +0100

    Energy update

--- src/energy/simulation/Energy.java
+++ src/energy/simulation/Energy.java
@@ -3,22 +3,21 @@ package energy.simulation;
 import GenCol.entity;
 
 public class Energy extends entity {
-	public String name;
-	public double energy;
+	private double energy;
 
-	public Energy(String name) {
-		super(name);
-		this.name = name;
-		this.energy = 10;
+	public Energy() {
+		this.energy = 0;
 	}
 
-	public Energy(String name, double energy) {
-		super(name);
-		this.name = name;
+	public Energy(double energy) {
 		this.energy = energy;
 	}
 
+	public double getEnergy() {
+		return this.energy;
+	}
+
 	public void print() {
-		System.out.println("Energy unit: " + name + " energy: " + energy);
+		System.out.println("Energy: " + energy);
 	}
 }
--- src/energy/simulation/LogicUnit.java
+++ src/energy/simulation/LogicUnit.java
@@ -20,7 +20,7 @@ public class LogicUnit extends ViewableAtomic {
 		addInport("inFromPVPanels");
 		addOutport("outToHouse");
 
-		addTestInput("in", new Energy("C1", 1000));
+		//addTestInput("in", new Energy(1000));
 	}
 
 	public void initialize() {
@@ -34,36 +34,21 @@ public class LogicUnit extends ViewableAtomic {
 		time += e;
 		Continue(e);
 
-		if (phaseIs("passive"))
+		if (phaseIs("active")) {
 			for (int i = 0; i < x.getLength(); i++) {
 				if (messageOnPort(x, "inFromPVPanels", i)) {
-					Energy receivedEnergy = (Energy) x.getValOnPort("inFromPVPanels", i); 
+					Energy receivedEnergy = (Energy) x.getValOnPort("inFromPVPanels", i);
 					this.energy = receivedEnergy;
 					return;
 				}
-
-				if (messageOnPort(x, "q_status", i)) {
-					signal q_status = (signal) x.getValOnPort("q_status", i);
-
-					if (q_status.name.equals("1")) {
-						holdIn("calling", 20);
-					}
-				}
 			}
+		}
 
 		System.out.println("external-Phase after: " + phase);
 	}
 
 	public void deltint() {
 		time += sigma;
-
-		if (phaseIs("calling")) {
-			passivate();
-		}
-
-		if (phaseIs("busy")) {
-			holdIn("calling", 20);
-		}
 	}
 
 	public void deltcon(double e, message x) {
@@ -74,61 +59,22 @@ public class LogicUnit extends ViewableAtomic {
 	public message out() {
 		message m = new message();
 
-		if (phaseIs("calling")) {
-			m.add(makeContent("next", (signal) new signal("1")));
-		}
-		if (phaseIs("busy")) {
-			this.energy.depart_time = time;
-			m.add(makeContent("out", (customer) this.energy));
+		if (phaseIs("active")) {
+			if (energy.getEnergy() != 0) {
+				m.add(makeContent("outToHouse", energy));
+			} else {
+				m.add(makeContent("outToHouse", new Energy()));
+			}
+
 		}
 		return m;
 	}
 
 	public void showState() {
 		super.showState();
-		// System.out.println("job: " + job.getName());
 	}
 
 	public String getTooltipText() {
 		return super.getTooltipText() + "\n" + "job: ";
 	}
-
-	/*
-	 * public void initialize() { phase = "passive"; sigma = INFINITY;
-	 * super.initialize(); clockTime = 0; }
-	 * 
-	 * public void deltext(double e, message x) { clockTime += e; Continue(e);
-	 * 
-	 * if (phaseIs("passive")) for (int i = 0; i < x.getLength(); i++) { if
-	 * (messageOnPort(x, "in", i)) { customer c = (customer) x.getValOnPort("in",
-	 * i); c.service_time = clockTime; this.customer = c; holdIn("busy",
-	 * c.processing_time); return; }
-	 * 
-	 * if (messageOnPort(x, "q_status", i)) { signal q_status = (signal)
-	 * x.getValOnPort("q_status", i);
-	 * 
-	 * if (q_status.name.equals("1")) { holdIn("calling", 20); } } }
-	 * 
-	 * System.out.println("external-Phase after: " + phase); }
-	 * 
-	 * public void deltint() { clockTime += sigma;
-	 * 
-	 * if (phaseIs("calling")) { passivate(); }
-	 * 
-	 * if (phaseIs("busy")) { holdIn("calling", 20); } }
-	 * 
-	 * public void deltcon(double e, message x) { deltint(); deltext(0, x); }
-	 * 
-	 * public message out() { message m = new message();
-	 * 
-	 * if (phaseIs("calling")) { m.add(makeContent("next", (signal) new
-	 * signal("1"))); } if (phaseIs("busy")) { this.customer.depart_time =
-	 * clockTime; m.add(makeContent("out", (customer) this.customer)); } return m; }
-	 * 
-	 * public void showState() { super.showState(); // System.out.println("job: " +
-	 * job.getName()); }
-	 * 
-	 * public String getTooltipText() { return super.getTooltipText() + "\n" +
-	 * "job: "; }
-	 */
 }
