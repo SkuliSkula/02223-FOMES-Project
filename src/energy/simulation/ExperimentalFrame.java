37b20d25d3706758ae3e9e8f4732f9bf8e7ff769
ZuzkaJelcicova <215107@via.dk>
2017-11-11 15:07:44 +0100

    All couplings and models

--- src/energy/simulation/ExperimentalFrame.java
+++ src/energy/simulation/ExperimentalFrame.java
@@ -1,5 +1,42 @@
 package energy.simulation;
 
-public class ExperimentalFrame {
+import view.modeling.ViewableAtomic;
+import view.modeling.ViewableDigraph;
+
+public class ExperimentalFrame extends ViewableDigraph {
+	private final double OBSERVATION_TIME = 1;
+
+	public ExperimentalFrame(String name) {
+		super(name);
+		experimentalFrameConstruct();
+	}
+
+	public ExperimentalFrame() {
+		super("ExperimentalFrame");
+		experimentalFrameConstruct();
+	}
+
+	private void experimentalFrameConstruct() {
+		addInport("start");
+		addInport("inFromHouse");
+		addInport("inFromGrid");
+		addOutport("outFromGenerator");
+		addOutport("outFromTrancducer");
+
+		ViewableAtomic generator = new Generator();
+		ViewableAtomic trancducer = new Trancducer("Trancducer", OBSERVATION_TIME);
+
+		add(generator);
+		add(trancducer);
+
+		initialize();
+
+		addCoupling(this, "start", generator, "start");
+		addCoupling(this, "inFromHouse", trancducer, "inFromHouse");
+		addCoupling(this, "inFromGrid", trancducer, "inFromGrid");
+		addCoupling(generator, "outFromEXPF", this, "outFromGenerator");
+		addCoupling(trancducer, "out", this, "outFromTrancducer");
+		addCoupling(trancducer, "out", generator, "stop");
+	}
 
 }
---
+++ src/energy/simulation/Simulator.java
@@ -0,0 +1,39 @@
+package energy.simulation;
+
+import GenCol.entity;
+import view.modeling.ViewableDigraph;
+
+public class Simulator extends ViewableDigraph {
+
+	public Simulator(String name) {
+		super(name);
+		simulatorConstruct();
+	}
+
+	public Simulator() {
+		super("HomeAutomationSystem");
+		simulatorConstruct();
+	}
+
+	private void simulatorConstruct() {
+
+		addInport("start");
+		addOutport("out");
+		
+		ViewableDigraph homeAutomationSystem = new HomeAutomationSystem("HomeAutomationSystem");
+		ViewableDigraph experimentalFrame  = new ExperimentalFrame("HomeAutomationSystem");
+
+		add(homeAutomationSystem);
+		add(experimentalFrame);
+		
+		addTestInput("start", new entity("start"));
+
+		initialize();
+
+		addCoupling(this, "start", experimentalFrame, "start");
+		addCoupling(experimentalFrame, "outFromTrancducer", this, "out");
+		addCoupling(homeAutomationSystem, "outFromGrid", experimentalFrame, "inFromGrid");
+		addCoupling(homeAutomationSystem, "outFromHouse", experimentalFrame, "inFromHouse");
+		addCoupling(experimentalFrame, "outFromGenerator", homeAutomationSystem, "in");
+	}
+}
