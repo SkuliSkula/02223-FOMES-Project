10f124ae4d4a36433ffd9d197b9c1e0744ddb99e
stookit <mardari.adri@gmail.com>
2017-11-11 13:02:21 +0100

    Battery + GRID

--- src/energy/simulation/Battery.java
+++ src/energy/simulation/Battery.java
@@ -1,114 +1,151 @@
 package energy.simulation;
 
-import GenCol.*;
+import GenCol.entity;
 import model.modeling.*;
 import view.modeling.ViewableAtomic;
 
 public class Battery extends ViewableAtomic { // ViewableAtomic is used instead
 												// of atomic due to its
 												// graphics capability
-	protected double storage;
-	protected double storage_max;
-	protected double storage_min;
+	private final static double BATTERY_CAPACITY = 13500;
+	private final static double MIN_PERCENT = 25, MAX_PERCENT = 85;
+	protected static final double INC_TIME = 1;
+	private double availableStorage;
+	private double stateOfCharge;
+	private double totalCapacity;
+	private int noOfBatteries;
 	protected double time;
-	double diff;
+	protected Energy excessEnergy;
+	protected Energy outputEnergy;
 
 	public Battery() {
-		this("Battery");
+		this("Battery", 3);
 	}
 
 	public Battery(String name) {
+		this(name, 3);
+	}
+
+	public Battery(String name, int noOfBatteries) {
 		super(name);
+		this.noOfBatteries = noOfBatteries;
+		this.totalCapacity = this.noOfBatteries * (BATTERY_CAPACITY * (MAX_PERCENT - MIN_PERCENT));
+		this.availableStorage = totalCapacity;
+		this.stateOfCharge = 0;
 		addInport("inFromLU"); // Charge the battery
-		addInport("inFromLURequest"); // Requested energy from House or Electric car
+		addInport("inFromLURequest"); // Requested energy from House or Electric
+										// car
 		addOutport("outExtraToLU");
 		addOutport("outToLU"); // Send the requested energy
+		
+		addTestInput("inFromLU", new Energy(5000));
+		addTestInput("inFromLU", new Energy(7000));
+		addTestInput("inFromLU", new Energy(8000));
+		addTestInput("inFromLU", new Energy(14000));
+		addTestInput("inFromLU", new Energy(3000));
+		addTestInput("inFromLU", new Energy(1000));
+		addTestInput("inFromLURequest", new Energy(1000));
+		addTestInput("inFromLURequest", new Energy(5000));
+		addTestInput("inFromLURequest", new Energy(6000));
+		addTestInput("inFromLURequest", new Energy(8000));
+		addTestInput("inFromLURequest", new Energy(12000));
+		addTestInput("inFromLURequest", new Energy(1000));
+		addTestInput("inFromLURequest", new Energy(5000));
 	}
 
 	public void initialize() {
-		phase = "passive";
+		phase = "IDLE";
 		sigma = INFINITY;
+		System.out.println("Initialize in battery!");
 		super.initialize();
 		time = 0;
-		storage_min = 135000 * 0.25;
-		storage_max = 135000 * 0.85;
-		diff = 0;
 	}
 
 	public void deltext(double e, message x) {
 		Continue(e);
 		time += e;
 
-		/*
-		 * signal 0 is passive signal 1 is active signal 2 is charging
-		 */
-		if (phaseIs("charging")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "in", i)) {
-					entity val = x.getValOnPort("in", i);
-					doubleEnt f = (doubleEnt) val;
-					if ((storage += f.getv()) <= storage_max) {
-						storage += f.getv();
-						holdIn("charging", 0);
-					} else if ((storage += f.getv()) > storage_max) {
-						storage += f.getv();
-						holdIn("passive", 0);
-					}
+		boolean charging = false;
+		System.out.println("External event received in battery!");
+		for (int i = 0; i < x.getLength(); i++) {
+			if (messageOnPort(x, "inFromLU", i)) {
+				Energy pvEnergy = (Energy) x.getValOnPort("inFromLU", i);
+				if (pvEnergy.getEnergy() <= availableStorage && pvEnergy.getEnergy() <= (noOfBatteries - 1) * 5000) {
+					charging = true;
+					availableStorage -= pvEnergy.getEnergy();
+					stateOfCharge += pvEnergy.getEnergy();
+				} else if (pvEnergy.getEnergy() <= availableStorage) {
+					charging = true;
+					availableStorage -= (noOfBatteries - 1) * 5000;
+					stateOfCharge += (noOfBatteries - 1) * 5000;
+					excessEnergy = new Energy(pvEnergy.getEnergy() - (noOfBatteries - 1) * 5000);
+				} else {
+					if (availableStorage != 0)
+						charging = true;
+					excessEnergy = new Energy(pvEnergy.getEnergy() - availableStorage);
+					availableStorage = 0;
+					stateOfCharge = totalCapacity;
 				}
-			}
-		} else if (phaseIs("passive")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "statusIn", i)) {
-					signal statusIn = (signal) x.getValOnPort("statusIn", i);
-					if (statusIn.name.equals("1")) {
-						holdIn("active", 0);
-					} else if (statusIn.name.equals("2")) {
-						holdIn("charging", 0);
-					}
-				}
-			}
-		} else if (phaseIs("active")) {
-			for (int i = 0; i < x.getLength(); i++) {
-				if (messageOnPort(x, "in", i)) {
-					entity val = x.getValOnPort("in", i);
-					doubleEnt f = (doubleEnt) val;
-					if ((storage -= f.getv()) > storage_min) {
-						storage -= f.getv();
-						diff = f.getv();
-						holdIn("active", 0);
-					} else if ((storage += f.getv()) <= storage_min) {
-						storage -= f.getv();
-						diff = f.getv();
-						holdIn("passive", 0);
-					}
-				}
-				if (messageOnPort(x, "statusIn", i)) {
-					signal statusIn = (signal) x.getValOnPort("statusIn", i);
-					if (statusIn.name.equals("0")) {
-						holdIn("passive", 0);
-					} else if (statusIn.name.equals("2")) {
-						holdIn("charging", 0);
-					}
+			} else if (messageOnPort(x, "inFromLURequest", i)) {
+				Energy reqEnergy = (Energy) x.getValOnPort("inFromLURequest", i);
+				if (reqEnergy.getEnergy() <= stateOfCharge) {
+					stateOfCharge -= reqEnergy.getEnergy();
+					availableStorage += reqEnergy.getEnergy();
+					outputEnergy = reqEnergy;
+				} else {
+					if (stateOfCharge != 0)
+						outputEnergy = new Energy(stateOfCharge);
+					stateOfCharge = 0;
+					availableStorage = totalCapacity;
 				}
 			}
 		}
+		if (charging) {
+			if (outputEnergy != null) {
+				holdIn("DJARGING", INC_TIME);
+			} else {
+				holdIn("CHARGING", INC_TIME);
+			}
+		} else {
+			if (outputEnergy != null) {
+				holdIn("DISCHARGING", INC_TIME);
+			} else {
+				holdIn("IDLE", INC_TIME);
+			}
+		}
+		//// Everything from above should probably be the case for all phases
 	}
 
 	public void deltint() {
+		System.out.println("Time incremented");
+		time += sigma;
 		// passivate();
 	}
 
 	public message out() {
 		message m = new message();
 
-		if (!phaseIs("charging") && storage < storage_min) {
-			m.add(makeContent("statusOut", new doubleEnt(storage)));
-			holdIn("passive", 0);
-		} else if (!phaseIs("active") && storage >= storage_max) {
-			m.add(makeContent("statusOut", new doubleEnt(storage)));
-			holdIn("passive", 0);
-		} else if (phaseIs("active")) {
-			m.add(makeContent("out", new doubleEnt(diff)));
+		if (!phaseIs("CHARGING")) {
+			if (excessEnergy != null) {
+				m.add(makeContent("outExtraToLU", excessEnergy));
+				excessEnergy = null;
+			} else {
+				holdIn("IDLE", 0);
+			}
+		} else if (phaseIs("DISCHARGING")) {
+			if (outputEnergy != null) {
+				m.add(makeContent("outToLU", outputEnergy));
+				outputEnergy = null;
+			}else{
+				m.add(makeContent("outToLU", new Energy()));
+			}
+		} else if (phaseIs("DJARGING")) {
+			if(excessEnergy != null){
+				m.add(makeContent("outExtraToLU", excessEnergy));
+				excessEnergy = null;
+			}
+			m.add(makeContent("outToLU", outputEnergy));
+			outputEnergy = null;
 		}
 		return m;
 	}
--- src/energy/simulation/ExternalPowerGrid.java
+++ src/energy/simulation/ExternalPowerGrid.java
@@ -1,10 +1,14 @@
 package energy.simulation;
 
-import GenCol.entity;
 import model.modeling.message;
 import view.modeling.ViewableAtomic;
 
 public class ExternalPowerGrid extends ViewableAtomic{
+	
+	protected Energy excess;
+	protected double totalExcess;
+	protected double time;
+	protected static final double INC_TIME = 1;
 
 	public ExternalPowerGrid() {
 		super("External Grid");
@@ -12,30 +16,49 @@ public class ExternalPowerGrid extends ViewableAtomic{
 	
 	public ExternalPowerGrid(String name) {
 		super(name);
-		
 		addInport("inFromLU");
-		addOutport("outToLU");
-		
+
+		this.time = 0;
 		initialize();
 	}
 	
 	public void initialize() {
-		phase = "active";
+		phase = "IDLE";
 		sigma = INFINITY;
 		super.initialize();
 	}
 
 	public void deltint() {
-
+		time += INC_TIME;
+		/*if(phaseIs("TAKING")){
+			holdIn("IDLE", INC_TIME);
+		}*/
 	}
 
-	// I receive 0 or smth
 	public void deltext(double e, message x) {
+		time += e;
+		Continue(e);
 		
+		if(phaseIs("IDLE") || phaseIs("TAKING")){
+			for (int i = 0; i < x.getLength(); i++) {
+				if (messageOnPort(x, "inFromLU", i)) {
+					Energy energy = (Energy) x.getValOnPort("inFromLU", i);
+					System.out.println("####### Energy received in the Power Grid: " + energy.getEnergy());
+					this.excess = energy;
+					this.totalExcess += energy.getEnergy();
+					holdIn("TAKING", INC_TIME);
+				}
+			}
+		}
 	}
 
 	public message out() {
 		message m = new message();
+		/*
+		if(phaseIs("TAKING")){
+			m.add(makeContent("outToTrancducer", excess));
+		}
+		*/
 		return m;
 	}
 
